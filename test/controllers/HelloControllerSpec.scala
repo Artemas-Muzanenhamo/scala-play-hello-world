993d84b5343484bea060c71f2e1e137f982e1da9
Artemas-Muzanenhamo <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2024-08-15 09:18:51 +0100

    amend test

--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -2,19 +2,21 @@ package controllers
 
 import org.scalatest.matchers.must.Matchers.mustBe
 import org.scalatestplus.play.PlaySpec
+import play.api.http.Status.OK
 import play.api.mvc.{Result, Results}
-import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout}
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, status}
 import play.api.test.{FakeRequest, Helpers}
 
 import scala.concurrent.Future
 
 class HelloControllerSpec extends PlaySpec with Results {
   "GET /" should {
-    "return 200" in new TestSetup {
+    "should return 200 and a body with value 'Hello'" in new TestSetup {
       val result: Future[Result] = controller.greeting().apply(FakeRequest())
       
+      status(result) mustBe OK
       val bodyText: String = contentAsString(result)
-      bodyText.mustBe("Hello")
+      bodyText mustBe "Hello"
     }
   }
 
