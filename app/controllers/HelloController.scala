7e3c2321b0217860684ce6fdf67b2cbb6a2f1a25
Artemas-Muzanenhamo <29547780+Artemas-Muzanenhamo@users.noreply.github.com>
2024-08-16 07:44:49 +0100

    add json value

--- app/controllers/HelloController.scala
+++ app/controllers/HelloController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents}
 
 import javax.inject.{Inject, Singleton}
@@ -7,6 +8,12 @@ import javax.inject.{Inject, Singleton}
 @Singleton
 class HelloController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   def greeting() = Action {
-    Ok("Hello")
+    val jsValue = Json.parse(
+      """
+        |{
+        |   "greeting": "Hello"
+        |}
+        |""".stripMargin)
+    Ok(jsValue)
   }
 }
--- test/controllers/HelloControllerSpec.scala
+++ test/controllers/HelloControllerSpec.scala
@@ -3,20 +3,21 @@ package controllers
 import org.scalatest.matchers.must.Matchers.mustBe
 import org.scalatestplus.play.PlaySpec
 import play.api.http.Status.OK
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Result, Results}
-import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, status}
+import play.api.test.Helpers.{contentAsJson, defaultAwaitTimeout, status}
 import play.api.test.{FakeRequest, Helpers}
 
 import scala.concurrent.Future
 
 class HelloControllerSpec extends PlaySpec with Results {
   "GET /" should {
-    "should return 200 and a body with value 'Hello'" in new TestSetup {
+    "should return 200 and a body with a JSON value 'Hello'" in new TestSetup {
       val result: Future[Result] = controller.greeting().apply(FakeRequest())
-      
+
       status(result) mustBe OK
-      val bodyText: String = contentAsString(result)
-      bodyText mustBe "Hello"
+      val bodyJsonValue: JsValue = contentAsJson(result)
+      bodyJsonValue mustBe Json.parse("{\"greeting\": \"Hello\"}")
     }
   }
 
